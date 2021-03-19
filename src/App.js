b4cade07a65f688d50fa43c5727f9714e6b3e4c0
Marcin <marcin.jakubkrysinski@gmail.com>
2021-03-19 19:40:43 +0100

    connected to api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Button } from "./components/Button";
 import { Converter } from "./components/Converter";
 
 function App() {
   return (
     <div className="App">
-        <p>Hello!!!</p>
+        <p>Hello!!! This is currency converter!!!</p>
         <Converter />
     </div>
   );
--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,29 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button} from '../Button';
 
-function Converter(){
+
+function Select({value, setCurrency}){
     return(
-        <form>
+        <select value={value} onChange={(event) =>
+            setCurrency(event.target.value)}>
+            <option value="PLN">PLN</option>
+            <option value="EUR">EUR</option>
+            <option value="USD">USD</option>
+            <option value="GBP">GBP</option>
+        </select>
+    );
+}
+
+function Converter() {
+    const [amount, setAmount] = useState(0);
+    const [result, setResult] = useState(0);
+    const [currencyFrom, setCurrencyFrom] = useState('PLN');
+    const [currencyTo, setCurrencyTo] = useState('EUR');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch(`https://api.ratesapi.io/api/latest?base=${currencyFrom}`)
+            .then(response => response.json())
+            .then(data => {
+                setResult(amount * data.rates[currencyTo]);
+            });
+    }
+    return (
+        <form onSubmit={handleSubmit}>
             <div>
-                <input type="number" placeholder="Amount" />
+                <input type="number" placeholder="Amount" onChange={
+                    (event) => setAmount(event.target.value)}/>
             </div>
             <div>
                 <span>From:</span>
-                <select>
-                    <option value="PLN">PLN</option>
-                    <option value="EUR">EUR</option>
-                    <option value="USD">USD</option>
-                    <option value="GBP">GBP</option>
-                </select>
+                <Select value={currencyFrom} setCurrency={setCurrencyFrom} />
             </div>
             <div>
                 <span>To:</span>
-                <select>
-                    <option value="PLN">PLN</option>
-                    <option value="EUR">EUR</option>
-                    <option value="USD">USD</option>
-                    <option value="GBP">GBP</option>
-                </select>
+                <Select value={currencyTo} setCurrency={setCurrencyTo} />
             </div>
+            <div>Result: {result}</div>
+            <Button type='submit'>Send</Button>
         </form>
     );
 }
